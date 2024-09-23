c8e4780598590db9030db50121233bf1b1a0cc04
evaldo.venceslau@hotmail.com <evaldo.venceslau@hotmail.com>
2024-09-23 17:46:41 -0300

    pwa

--- front/script.js
+++ front/script.js
@@ -13,4 +13,15 @@ btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
 })
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('service-worker.js')
+            .then(registration => {
+                console.log('Service Worker registrado com sucesso:', registration);
+            })
+            .catch(error => {
+                console.log('Falha ao registrar o Service Worker:', error);
+            });
+    });
+}
 
